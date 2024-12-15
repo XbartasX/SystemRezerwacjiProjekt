a631ed806987ca0810d515526a822799f88b44d6
bartek <bartek@DESKTOP-LQCJL0D>
2024-12-15 21:25:45 +0100

    walidacja

--- SystemRezerwacjiProjekt/Models/Doctor.cs
+++ SystemRezerwacjiProjekt/Models/Doctor.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SystemRezerwacjiProjekt.Models
 {
     public class Doctor
     {
         public int Id { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Imię może zawierać tylko litery.")]
+        [StringLength(30, ErrorMessage = "Imię może mieć maksymalnie 30 znaków.")]
         public string Name { get; set; }
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Nazwisko może zawierać tylko litery.")]
+        [StringLength(30, ErrorMessage = "Nazwisko może mieć maksymalnie 30 znaków.")]
         public string Surname { get; set; }
     }
 }
--- SystemRezerwacjiProjekt/Models/Patient.cs
+++ SystemRezerwacjiProjekt/Models/Patient.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SystemRezerwacjiProjekt.Models
 {
     public class Patient
     {
         public int Id { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Imię może zawierać tylko litery.")]
+        [StringLength(30, ErrorMessage = "Imię może mieć maksymalnie 30 znaków.")]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Imię może zawierać tylko litery.")]
+        [StringLength(30, ErrorMessage = "Imię może mieć maksymalnie 30 znaków.")]
         public string Surname { get; set; }
+
         public string? UserId {  get; set; }
         public IdentityUser? User { get; set; }
     }
